702e6717dfdf91787c3e01e34788d9af217c84c1
SammySlimJ <samanthasalimu@gmail.com>
2024-10-29 11:45:47 -0700

    Implement Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,52 @@
 #include "sort.h"
-#include <stdio.h>
 
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp, *insert_after;
+	listint_t *iter, *insert, *tmp;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = (*list)->next;
-
-	while (current != NULL)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		temp = current;
-		insert_after = current->prev;
+		tmp = iter->next;
+		insert = iter->prev;
 
-		while (insert_after != NULL && insert_after->n > temp->n)
+		while (insert != NULL && iter->n < insert->n)
 		{
-			if (insert_after->next != temp)
-			{
-				if (insert_after->prev)
-					insert_after->prev->next = temp;
-				temp->prev = insert_after->prev;
-
-				insert_after->next = temp->next;
-				if (temp->next)
-					temp->next->prev = insert_after;
-
-				temp->next = insert_after;
-				insert_after->prev = temp;
-
-				if (temp->prev == NULL)
-					*list = temp;
-
-				print_list(*list);
-			}
-			insert_after = insert_after->prev;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		current = current->next;
 	}
 }
